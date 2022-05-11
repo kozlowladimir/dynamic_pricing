d4307678320657daf39c39277251b9fef2e1d104
kozlowladimir <kozlowladimir@gmail.com>
2022-05-11 11:54:34 +0700

    Рефакторинг класса PricingSomeMethod, добавление класса AbstractPricingSomeMethod

--- app/utils/pricing.py
+++ app/utils/pricing.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 import numpy as np
 import pandas as pd
 from default_strategy import default_strategy
@@ -7,6 +9,44 @@ def ceil_to_base(x, base=2):
     if x == 0:
         return base
     return int(base * np.ceil(x / base))
+
+
+class AbstractPricingSomeMethod(ABC):
+    """
+    Абстрактый класс для реализации различных стратегий ценообразования
+    """
+
+    def __init__(self, nominal_price, threshold, explore_count=500):
+        self.BAR = nominal_price
+        self.RackRate = 1.5 * nominal_price
+        self.Netto = 0.5 * nominal_price
+        self.history = pd.DataFrame(columns=['price', 'acceptance'])
+        self.price_steps_count = 40
+        self.price_step = (self.RackRate - self.Netto) / self.price_steps_count
+        self.prices = [self.Netto + x * self.price_step for x in range(self.price_steps_count + 1)]
+        self.average_period = 7
+        self.event_queue = {x: [0] for x in range(31)}
+        self.threshold = threshold
+        self.explore_count = explore_count
+
+    def update_history(self, event):
+        self.history = pd.concat([self.history, pd.DataFrame(event, index=[0])])
+
+    def update_queue(self, events_dict):
+        for key in events_dict:
+            if len(self.event_queue[key]) == 7:
+                self.event_queue[key].pop(0)
+            self.event_queue[key].append(events_dict[key])
+
+    def get_average_orders(self, depth):
+        result = 0
+        for x in range(depth + 1):
+            result += sum(self.event_queue[x])
+        return result
+
+    @abstractmethod
+    def set_price(self, hotel, request, verbose=0):
+        pass
 
 
 class PricingDefault:
@@ -57,41 +97,20 @@ class PricingRandom:
         return self.Netto + rand_int * self.price_step
 
 
-class PricingSomeMethod:
+class PricingSomeMethod(AbstractPricingSomeMethod):
     """
-    Дефолтная стратегия ценообразования
+    Класс, описывающий одну из стратегий ценообразования
     """
 
-    def __init__(self, nominal_price, threshold):
-        self.BAR = nominal_price
-        self.RackRate = 1.5 * nominal_price
-        self.Netto = 0.5 * nominal_price
-        self.history = pd.DataFrame(columns=['price', 'acceptance'])
-        self.price_steps_count = 40
-        self.price_step = (self.RackRate - self.Netto) / self.price_steps_count
-        self.prices = [self.Netto + x * self.price_step for x in range(self.price_steps_count + 1)]
-        self.average_period = 7
-        self.event_queue = {x: [0] for x in range(31)}
-        self.threshold = threshold
-
-    def update_history(self, event):
-        self.history = pd.concat([self.history, pd.DataFrame(event, index=[0])])
-
-    def update_queue(self, events_dict):
-        for key in events_dict:
-            if len(self.event_queue[key]) == 7:
-                self.event_queue[key].pop(0)
-            self.event_queue[key].append(events_dict[key])
-
-    def get_average_orders(self, depth):
-        result = 0
-        for x in range(depth + 1):
-            result += sum(self.event_queue[x])
-        return result
+    def calc_price(self, rs: pd.DataFrame) -> int:
+        if not len(rs[rs > self.threshold]):
+            return rs.index[-1]
+        else:
+            return rs[rs > self.threshold].index[0]
 
     def set_price(self, hotel, request, verbose=0):
 
-        if len(self.history) > 500:
+        if len(self.history) > self.explore_count + len(self.prices):
             rest = self.get_average_orders(request.depth) / (
                         hotel.number_of_rooms - hotel.get_loading(request.start_day))
 
@@ -103,10 +122,7 @@ class PricingSomeMethod:
                 print(f'rest: {rest}')
                 print(rs)
 
-            if not len(rs[rs > self.threshold]):
-                price = rs.index[-1]
-            else:
-                price = rs[rs > self.threshold].index[0]
+            price = self.calc_price(rs)
         else:
             price = self.prices[np.random.randint(len(self.prices))]
         return price
